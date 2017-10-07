3b6ec374f1a5a1fdbd6b578bf1d7f18f0c75b627
Wassana <wassana.name@g.swu.ac.th>
2017-10-07 14:29:07 +0700

    UPDATE

--- config/env/development.js
+++ config/env/development.js
@@ -3,5 +3,10 @@ module.exports = {
     // or use 
     mongoUri: 'mongodb://admin:password@localhost:27017/testDB1',
     debug: true,
-    sessionSecret: 'dev_secret_key'
+    sessionSecret: 'dev_secret_key',
+    google: {
+        clientID: '422328451945-bphjlt19lm3pee6r0qsog7lnifo4bm3e.apps.googleusercontent.com',
+        clientSecret: 'vqMWwGlyiAye0vTxNz9rtRfQ',
+        callbackURL: 'http://localhost:3000/oauth/google/callback'
+    }
 }
